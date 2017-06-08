07cba79342f37edb11b1c55bde54ba363d5ed662
tiMorpheus <timur10645@icloud.com>
2017-06-08 19:57:46 +0300

    init commit

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "tymurtolochko@gmail.com";
+$sitename = "test";
+
+$name = trim($_GET["name"]);
+$phone = trim($_GET["phone"]);
+
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $name \nТелефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
